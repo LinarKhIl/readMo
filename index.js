32b441a34b3d1c628ff70740b3a28a26a4acd48c
LinarKhil <zzlinarzz@gmail.com>
2018-08-07 21:17:46 +0500

    Register page added, Login page design updated

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res)
+app.use(express.static(__dirname));
+app.get('/', (req, res) =>
 	{
 		res.sendFile(__dirname + '/views/login_screen/index.html')
-	});
-app.post('/login', 
-	passport.authenticate('local'),
-	function(req, res)
-	{
-		res.redirect('/users/' + req.user.username);
 	});
 
 app.listen(3000, () => console.log('listen port is 3000'))
